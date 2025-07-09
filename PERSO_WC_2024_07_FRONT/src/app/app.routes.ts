5c0afaff48ef1e652620767717cda949eb3b9778
Dubut <dubut@et.esiea.fr>
2025-07-09 23:15:35 +0200

    :rocket: Added routes for nav bar in header

--- PERSO_WC_2024_07_FRONT/src/app/app.routes.ts
+++ PERSO_WC_2024_07_FRONT/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { AllegianceListComponent } from './components/allegiance-list/allegiance-list.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: "home", component: HomepageComponent},
-    { path: "login", component: LoginPageComponent}
+    { path: "login", component: LoginPageComponent},
+    { path: "clans", component: AllegianceListComponent},
+    { path: "maps", component: LoginPageComponent},
+    { path: "life", component: AllegianceListComponent},
+    { path: "bonuses", component: LoginPageComponent}
 ];
